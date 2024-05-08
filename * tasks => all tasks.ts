eb5eb470220c84480a0e9d0b77fdd27e67e1af34
ThreeNg2207 <>
2024-05-08 17:10:04 +0700

    No message

---
+++ * tasks => all tasks.ts
@@ -0,0 +1,15 @@
+* user => infor user
+* tasks => all tasks
+- when add task => update task and category
+* category => show home 
+- filter task by date (today)
+* Today task
+- 7 day off week
+- Press date show task
+- icon time show modal date get date of week and task of date select and active button
+- list task:
++ complete => opacity
++ inprogress => color main
++ todo => color main opacity
+
+
